ee55482a644b1afacf990d741ab93e148b3afd38
Ebadulislam <ebadulislamfarooqi@gmail.com>
2024-08-23 20:57:23 +0530

    added remaining time display

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // defining constants
+const DAY_NUMBER = dayOfYear() + 1; // (+1) to take the table header into account
+const SHEET_ID = "1V3c2-kDkehR_ViJdsJsgWkpK9vAn233j6Gm7lPOVueM";
+const SHEET_TITLE = "annual_time_table";
+const SHEET_RANGE = "A" + DAY_NUMBER + ":G" + DAY_NUMBER;
+const FULL_URL = "https://docs.google.com/spreadsheets/d/" + SHEET_ID + "/gviz/tq?sheet=" + SHEET_TITLE + "&range=" + SHEET_RANGE;
+
 function dayOfYear() {
     const now = new Date();
     const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -7,31 +13,29 @@ function dayOfYear() {
         dayNumber += daysInMonth[i];
     }
     dayNumber += now.getDate();
-    console.log(dayNumber);
+    // console.log(dayNumber);
     return dayNumber;
 }
-const DAY_NUMBER = dayOfYear() + 1;
-const SHEET_ID = "1V3c2-kDkehR_ViJdsJsgWkpK9vAn233j6Gm7lPOVueM";
-const SHEET_TITLE = "annual_time_table";
-const SHEET_RANGE = "A" + DAY_NUMBER + ":G" + DAY_NUMBER;
-const FULL_URL = "https://docs.google.com/spreadsheets/d/" + SHEET_ID + "/gviz/tq?sheet=" + SHEET_TITLE + "&range=" + SHEET_RANGE;
 
 window.onload = async function () {
+    timeRemaining();
+    setInterval(timeRemaining, 1000);
     try {
         const table = await fetchDataFromSheet();
-        console.log(table);
+        // console.log(table);
 
         document.getElementById("time-table").innerHTML = "";
+
         const eventNames = ["Today", "Fajr", "Sunrise", "Zuhr", "Asr", "Maghrib", "Isha"];
 
         for (let i = 1; i < table.length; i++) {
             let event = document.createElement("div");
             event.id = eventNames[i];
-            if (i % 2 == 0) {
-                event.classList.add("flex", "flex-row", "m-1", "bg-gray-400");
-            } else {
-                event.classList.add("flex", "flex-row", "m-1", "bg-gray-500");
-            }
+
+            (i % 2 == 0)
+                ? event.classList.add("flex", "flex-row", "m-1", "bg-gray-400")
+                : event.classList.add("flex", "flex-row", "m-1", "bg-gray-500");
+
             document.getElementById("time-table").append(event);
 
             let name = document.createElement("div");
@@ -41,7 +45,7 @@ window.onload = async function () {
 
             let time = document.createElement("div");
             time.classList.add("w-32", "md:w-64")
-            time.innerText = table[i].name;
+            time.innerText = table[i].time;
             document.getElementById(eventNames[i]).append(time);
         }
     } catch (error) {
@@ -59,11 +63,55 @@ async function fetchDataFromSheet() {
         const dataJson = rep.substr(47).slice(0, -2);
         const data = JSON.parse(dataJson);
         const table = data.table.rows[0].c.map((row) => ({
-            name: row.f,
+            time: row.f,
         }));
         return table;
     } catch (error) {
         console.error('Error fetching sheet data:', error);
         throw error;
     }
+}
+
+async function timeRemaining() {
+    try {
+        document.getElementById("remaining-time").innerText = "";
+        const now = new Date();
+        const currentHour = now.getHours();
+        const currentMinute = now.getMinutes();
+        const currentTimeInMinutes = currentHour * 60 + currentMinute;
+        // console.log("Current time: " + currentTimeInMinutes);
+        
+        const table = await fetchDataFromSheet();
+
+        let remainingTimeInMinutes = 1440 - currentTimeInMinutes + minuteUnits(table[1].time, false);
+        for (let i = 1; i < table.length; i++) {
+            const eventTimeInMinutes = minuteUnits(table[i].time, i>3);
+            // console.log("Event time: " + eventTimeInMinutes);
+            if(eventTimeInMinutes > currentTimeInMinutes) {
+                remainingTimeInMinutes = eventTimeInMinutes - currentTimeInMinutes;
+                // console.log("Time remaining: " + timeRemaining);
+                break;
+            }
+        }
+        // console.log("Time remaining: " + remainingTimeInMinutes);
+        const remainingTimeToBeDisplayed = displayFormat(remainingTimeInMinutes);
+        // console.log("Time remaining: " + remainingTimeToBeDisplayed);
+
+        document.getElementById("remaining-time").innerText = remainingTimeToBeDisplayed;
+
+    } catch (error) {
+        console.error("Error occurred in timeRemaining function:", error);
+        return;
+    }
+}
+
+function minuteUnits(time, isPM) {
+    const [hour, minute] = time.split(":");
+    return (Number(hour)+ (isPM ? 12 : 0)) * 60 + Number(minute);
+}
+
+function displayFormat(remainingTimeInMinutes) {
+    const hours = Math.floor(remainingTimeInMinutes / 60).toString().padStart(2, '0');
+    const minutes = (remainingTimeInMinutes % 60).toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
 }
